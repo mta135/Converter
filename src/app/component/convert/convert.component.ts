8edfd3cbd14fe376ec4d32020e17ac20fcf3462b
mihaitamazlicaru1959 <mihaitamazlicaru1959@outlook.com>
2024-06-29 12:39:44 +0300

    Repository.

--- src/app/component/convert/convert.component.ts
+++ src/app/component/convert/convert.component.ts
@@ -1,6 +1,8 @@
 import { Component } from "@angular/core";
 import { MaterialModule } from "../../helper/material.module";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { CurrencyRateRepository } from "../../repository/currencyrate.repository";
+import { CurrencyRate } from "../../models/currencyrate/currencyrate.model";
 
 @Component({
     selector: "convert",
@@ -8,11 +10,20 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
     standalone: true,
     styleUrl: './convert.component.scss',
     imports: [MaterialModule, FormsModule, ReactiveFormsModule],
+
 })
 
 export class ConvertComponent {
 
+    constructor(private currencyRate: CurrencyRateRepository) { }
+
+    get currencyRates(): CurrencyRate[] {
+        return this.currencyRate.GetCurrencyRates();
+    }
+
     TestClick() {
+        debugger;
+        var value = this.currencyRate.GetCurrencyRates();
         alert("This is an alert...")
     }
 
