e046c5c0aafade539a7ac31826860e0ebe91f9ea
mihaitamazlicaru1959 <mihaitamazlicaru1959@outlook.com>
2024-06-29 12:58:51 +0300

    Remove unused method

--- src/app/helper/convert.helper.ts
+++ src/app/helper/convert.helper.ts
@@ -1,14 +1,6 @@
-import { Currency, CurrencyRate } from "../models/currencyrate/currencyrate.model";
+import { CurrencyRate } from "../models/currencyrate/currencyrate.model";
 
 export class Helper {
-
-    // public SetDefaultCurrencyRates(name: string, currencies: Currency[]): Currency[] {
-    //     let tempCurrency: Currency[] = [];
-    //     let rate = currencies.find(x => x.CurrencyName == name)?.CurrencyRate;
-
-    //     return tempCurrency;
-    // }
-
 
     SetCurrencyRate(data: any): CurrencyRate[] {
         var currencyRates: CurrencyRate[] = [];
--- src/app/models/currencyrate/currencyrate.model.ts
+++ src/app/models/currencyrate/currencyrate.model.ts
@@ -1,27 +1,4 @@
-//#region Nu stiu daca pe viitor se vor folosi aceste clase...
 
-export interface Currency {
-    CurrencyRate: number;
-    CurrencyName: string;
-
-}
-
-
-
-export class CurrencyModel {
-    CurrencyRate: number;
-    CurrencyName: string;
-
-    constructor() {
-        this.CurrencyName = "";
-        this.CurrencyRate = 0;
-    }
-}
-
-//#endregion
-
-
-// clasa care se va folosi ...
 export class CurrencyRate {
     constructor(CurrencyRate: number, CurrencyName: string) { }
 
