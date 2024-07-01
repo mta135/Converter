a32d557b3e484b023e39a39eb8ddb58368619fd5
mihaitamazlicaru1959 <mihaitamazlicaru1959@outlook.com>
2024-07-01 21:58:51 +0300

    Add space

--- src/app/models/convert/convert.model.ts
+++ src/app/models/convert/convert.model.ts
@@ -3,12 +3,17 @@ import { CurrencyRate } from "../currencyrate/currencyrate.model";
 export class ConvertModel {
 
     public currencyRates: CurrencyRate[] = [];
+
     public FromDefaultId: number | undefined;
+
     public ToDefaultId: number | undefined;
 
     public fromCurrency: string = "";
+
     public toCurrency: string = "";
+
     public fromCurrencyId: string = "";
+
     public toCurrencyId: string = "";
 
 }
