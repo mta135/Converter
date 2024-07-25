53d6c95bc96c4773d4a14deb66bbec5bf8b0d182
mihaitamazlicaru1959 <mihaitamazlicaru1959@outlook.com>
2024-07-25 12:13:12 +0300

    Transfer files

--- src/app/component/exchange/exchange.component.ts
+++ src/app/component/exchange/exchange.component.ts
@@ -1,25 +1,153 @@
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { CurrencyRateRepository } from "../../repository/currencyrate.repository";
-import { Component } from "@angular/core";
+import { ChangeDetectorRef, Component } from "@angular/core";
+import { ConvertModel } from "../../models/convert/convert.model";
+import { ConvertTypeEnum } from "../../models/enums/convertype.enum";
+import { Helper } from "../../helper/convert.helper";
+import { CurrencyRate } from "../../models/currencyrate/currencyrate.model";
+import { MaterialModule } from "../../helper/material.module";
 
 @Component({
     selector: "convert",
     templateUrl: 'exchange.component.html',
     standalone: true,
     styleUrl: './exchange.component.scss',
-    imports: [FormsModule, ReactiveFormsModule],
+    imports: [MaterialModule, FormsModule, ReactiveFormsModule],
     providers: [CurrencyRateRepository]
 })
 
 
 export class ExchangeComponent {
 
-    public Checked(): boolean {
-        return false;
+    public convertModel = new ConvertModel();
+
+    constructor(currencyRateRepository: CurrencyRateRepository, private cdr: ChangeDetectorRef) {
+        this.SetCurrencyRates(currencyRateRepository);
     }
 
-    public RadioGroupClick(position: string): void {
-        console.log(position);
+    private SetCurrencyRates(currencyRateRepository: CurrencyRateRepository): void {
+        currencyRateRepository.GetCurrencyRates().subscribe(data => {
+
+            this.convertModel.currencyRates = Helper.ConvertToCurrencyRates(data);
+            this.SetRatesDefaultValues(this.convertModel.currencyRates);
+        });
+    }
+
+    private SetRatesDefaultValues(currencyRate: CurrencyRate[]): void {
+
+        this.convertModel.FromDefaultId = Helper.GetCurrencyRateId(currencyRate, "EUR");
+        this.convertModel.ToDefaultId = Helper.GetCurrencyRateId(currencyRate, "MDL");
+    }
+
+    //#region  input text box
+
+    GetFromCurrencyValue(): void {
+        if (this.convertModel.fromCurrency.length !== 0) {
+
+            if (!Helper.IsDigit(this.convertModel.fromCurrency)) {
+                this.ChangeDetector();
+                this.Alert("Acest simbol: (" + this.convertModel.fromCurrency + ") nu este permis. Se accepta doar cifre. ", this.convertModel, ConvertTypeEnum.FromCurrency);
+                return;
+            }
+            else {
+                var resultFromCurrency: number = this.ConvertFromCurrency(parseInt(this.convertModel.fromCurrency), this.convertModel.toCurrencyId);
+
+                if (isNaN(resultFromCurrency))
+                    this.ClearInputField(ConvertTypeEnum.ToCurrency, this.convertModel);
+                else
+                    this.convertModel.toCurrency = resultFromCurrency.toFixed(2).toString();
+            }
+
+        } else {
+            this.ClearInputField(ConvertTypeEnum.ToCurrency, this.convertModel);
+        }
+
+    }
+
+    GetToCurrency() {
+        if (this.convertModel.toCurrency.length !== 0) {
+
+            if (!Helper.IsDigit(this.convertModel.toCurrency)) {
+                this.ChangeDetector();
+                this.Alert("Acest simbol: (" + this.convertModel.toCurrency + ") nu este permis. Se accepta doar cifre. ", this.convertModel, ConvertTypeEnum.ToCurrency);
+                return;
+            }
+            else {
+                var resultFromCurrency: number = this.ConvertToCurrency(parseInt(this.convertModel.toCurrency), this.convertModel.fromCurrencyId);
+
+                if (isNaN(resultFromCurrency))
+                    this.ClearInputField(ConvertTypeEnum.FromCurrency, this.convertModel);
+                else
+                    this.convertModel.fromCurrency = resultFromCurrency.toFixed(2).toString();
+            }
+
+        } else {
+            this.ClearInputField(ConvertTypeEnum.FromCurrency, this.convertModel);
+        }
+    }
+
+    //#endregion
+
+    //#region Drop Down
+
+    onFoodSelection1() {
+        debugger;
+        var vaalue = this.convertModel.currencyRates;
+        console.log(this.convertModel.fromCurrencyId);
+    }
+
+    onFoodSelection2() {
+        debugger;
+        var vaalue = this.convertModel.currencyRates;
+        console.log(this.convertModel.fromCurrencyId);
+    }
+
+    //#endregion
+
+    private ConvertFromCurrency(from: number, toRateId: string): number {
+
+        var helper = new Helper();
+        var currency = helper.GetCurrencyById(this.convertModel.currencyRates, parseInt(toRateId));
+        var rate = currency.CurrencyRate;
+
+        var result = from * rate;
+
+        return result;
+    }
+
+
+    private ConvertToCurrency(from: number, toRateId: string): number {
+
+        var helper = new Helper();
+        var currency = helper.GetCurrencyById(this.convertModel.currencyRates, parseInt(toRateId));
+        var rate = currency.CurrencyRate;
+
+        var result = from / rate;
+
+        return result;
+    }
+
+
+    private ChangeDetector() {
+        this.cdr.detectChanges();
+    }
+
+
+    private Alert(alertText: string, convModel: ConvertModel, converType: ConvertTypeEnum): void {
+        alert(alertText);
+        this.ClearInputField(converType, convModel);
+    }
+
+    private ClearInputField(converType: ConvertTypeEnum, convModel: ConvertModel): void {
+
+        switch (converType) {
+            case ConvertTypeEnum.FromCurrency:
+                convModel.fromCurrency = "";
+                break;
+
+            case ConvertTypeEnum.ToCurrency:
+                convModel.toCurrency = "";
+        }
 
     }
 
